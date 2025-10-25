478fa755263c794aaa5bf82ccfb2f81287e06e48
Dombi Balazs Sandor <dombi.balazs.sandor@gmail.com>
2025-10-25 09:13:02 +0200

    small changes

--- cell_optimisation.jl
+++ cell_optimisation.jl
@@ -25,10 +25,10 @@ function crossover(top_half_population, bottom_half_population)
     num_offspring = size(bottom_half_population, 1)
     new_chromosomes = Array{Int64}(undef, num_offspring, 5)
     for i in 1:num_offspring
-        parent1_idx = rand(1:size(top_genes, 1))
-        parent2_idx = rand(1:size(top_genes, 1))
-        parent1 = top_genes[parent1_idx, :]
-        parent2 = top_genes[parent2_idx, :]
+        parent1_index = rand(1:size(top_genes, 1))
+        parent2_index = rand(1:size(top_genes, 1))
+        parent1 = top_genes[parent1_index, :]
+        parent2 = top_genes[parent2_index, :]
         child_genes = [parent1[1:3]; parent2[4:5]]
         new_chromosomes[i, :] = child_genes
     end
@@ -62,7 +62,7 @@ function cell_optimisation()
         population = vcat(top_half[:, 1:5], mutated_chromosomes)
     end
     final_ranked_population = fitness(population)
-    println("Best fitness found: ", final_ranked_population[1, 6])
+    println("Best fitness found: ", final_ranked_population[1, 6], "which genes are ", final_ranked_population[1, 1:5])
 end
 
 cell_optimisation()
